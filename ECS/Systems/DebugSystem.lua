5113a1bcc4f4c073856b4a0a7ef30f21368c4cc9
Callgage <gageprod@gmail.com>
2024-02-16 15:54:01 -0800

    Can now toggle viewing combat attack ranges

--- ECS/Systems/DebugSystem.lua
+++ ECS/Systems/DebugSystem.lua
@@ -5,18 +5,20 @@
 -- Some functions can be called via console commands.
 --
 
-return function(concord)
+return function (concord)
     local DebugSystem = concord.system({
-        rangeEntities = { "combatProperties", "position" }
+        rangeEntities = { 'combatProperties', 'position' }
     })
+
 
     ----------------------------
     -- [[ Public Functions ]] --
     ----------------------------
     function DebugSystem:toggleRanges()
         self.showRanges = not self.showRanges
-        console:log("Ranges toggled " .. (self.showRanges and "on" or "off"))
+        console:log('Ranges toggled ' .. (self.showRanges and 'on' or 'off'))
     end
+
 
     --------------------------
     -- [[ Core Functions ]] --
@@ -24,6 +26,28 @@ return function(concord)
     function DebugSystem:init()
         self.showRanges = false
     end
+    function DebugSystem:draw()
+        self:_drawRanges()
+    end
 
+
+    -----------------------------
+    -- [[ Private Functions ]] --
+    -----------------------------
+    function DebugSystem:_drawRanges()
+        if not self.showRanges then return end
+
+        for _,entity in ipairs(self.rangeEntities) do
+            local combatProperties = entity:get('combatProperties')
+            local position = entity:get('position')
+            local dimensions = entity:get('dimensions')
+            love.graphics.circle(
+                'line',
+                position.x,
+                position.y + dimensions.height / 2,
+                combatProperties.range
+            )
+        end
+    end
     return DebugSystem
 end

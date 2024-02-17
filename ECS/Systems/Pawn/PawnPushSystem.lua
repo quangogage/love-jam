988e2d172969fa66601098b891ec37ee92188b29
Callgage <gageprod@gmail.com>
2024-02-17 05:48:30 -0800

    Friendly and hostile pawns only push away those with the same ownership

--- ECS/Systems/Pawn/PawnPushSystem.lua
+++ ECS/Systems/Pawn/PawnPushSystem.lua
@@ -11,30 +11,34 @@ local PUSH_FORCE = 1000
 
 return function (concord)
     local PawnPushSystem = concord.system({
-        entities = { 'position', 'dimensions', 'physics' }
+        friendly = { 'position', 'dimensions', 'physics', 'friendly' },
+        hostile = { 'position', 'dimensions', 'physics', 'hostile' }
     })
 
     function PawnPushSystem:update(dt)
         local world = self:getWorld()
-        for i = 1, #self.entities do
-            local e1 = self.entities[i]
-            for j = i + 1, #self.entities do
-                local e2 = self.entities[j]
-                local e1Rect = self:_getCollisionRectangle(e1)
-                local e2Rect = self:_getCollisionRectangle(e2)
-                if self:_isColliding(e1Rect, e2Rect) then
-                    local angle = math.atan2(
-                        e2Rect.y - e1Rect.y,
-                        e2Rect.x - e1Rect.x
-                    )
-                    world:emit('physics_applyForce', e1,
-                        math.cos(angle) * -PUSH_FORCE,
-                        math.sin(angle) * -PUSH_FORCE
-                    )
-                    world:emit('physics_applyForce', e2,
-                        math.cos(angle) * PUSH_FORCE,
-                        math.sin(angle) * PUSH_FORCE
-                    )
+        for i = 1, 2 do
+            local entities = i == 1 and self.friendly or self.hostile
+            for i = 1, #entities do
+                local e1 = entities[i]
+                for j = i + 1, #entities do
+                    local e2 = entities[j]
+                    local e1Rect = self:_getCollisionRectangle(e1)
+                    local e2Rect = self:_getCollisionRectangle(e2)
+                    if self:_isColliding(e1Rect, e2Rect) then
+                        local angle = math.atan2(
+                            e2Rect.y - e1Rect.y,
+                            e2Rect.x - e1Rect.x
+                        )
+                        world:emit('physics_applyForce', e1,
+                            math.cos(angle) * -PUSH_FORCE,
+                            math.sin(angle) * -PUSH_FORCE
+                        )
+                        world:emit('physics_applyForce', e2,
+                            math.cos(angle) * PUSH_FORCE,
+                            math.sin(angle) * PUSH_FORCE
+                        )
+                    end
                 end
             end
         end

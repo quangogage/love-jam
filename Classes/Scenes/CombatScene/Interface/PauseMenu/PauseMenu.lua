bff5ae4ffd34f9bc1edadfc3ed321ce6d12b4932
Callgage <gageprod@gmail.com>
2024-02-19 12:55:00 -0800

    About to start on the pause menu

---
+++ Classes/Scenes/CombatScene/Interface/PauseMenu/PauseMenu.lua
@@ -0,0 +1,22 @@
+---@author Gage Henderson 2024-02-19 12:50
+--
+--
+
+---@class PauseMenu
+local PauseMenu = Goop.Class({
+    static = {
+        active = false,
+        backgroundOverlay = {
+            alpha = 0,
+            fadeSpeed = 2
+        }
+    },
+})
+
+
+--------------------------
+-- [[ Core Functions ]] --
+--------------------------
+
+
+return PauseMenu

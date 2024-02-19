066e495eb2816083ea7ed1e86c00c4e77ba051f7
Callgage <gageprod@gmail.com>
2024-02-18 20:37:22 -0800

    Cleaned up powerups a bit

--- lists/powerups.lua
+++ lists/powerups.lua
@@ -2,26 +2,42 @@
 --
 -- Must have at least 3.
 --
--- See `Classes.Scenes.CombatScene.PowerupSelectionMenu.PowerupSelectionMenu`
+-- This table is mapped to a table for each pawn type in PowerupStateManager.
+--
+-- Whenever a pawn is created, it is given a deep copy of that table stored in
+-- `pawn.powerups`.
 
+local Powerup  = require("Classes.Types.Powerup")
 return {
-    {
-        name = "Fast Walker",
-        description = "Move 20% faster",
-        ---@param self table
+    Powerup({
+        name = 'Fast Walker',
+        description = 'Move 20% faster',
+
+        ---@param self Powerup
         ---@param pawn BasicPawn | Pawn | table
-        onPawnCreation = function(self, pawn)
-            for _=1, self.count do
+        onPawnCreation = function (self, pawn)
+            for _ = 1, self.count do
                 pawn.movement.walkSpeed = pawn.movement.walkSpeed * 1.20
             end
         end
-    },
-    {
-        name = "Bloodlust",
-        description = "Deal 15% more damage"
-    },
-    {
-        name = "Tough Skin",
-        description = "Take 15% less damage"
-    }
+    }),
+    Powerup({
+        name = 'Bloodlust',
+        description = 'Deal 15% more damage',
+
+        -- Keep this powerup behavior somewhat ambiguous so it can be applied
+        -- to different things in the future...
+        ---@param self Powerup
+        ---@param value number
+        getMultipliedValue = function (self, value)
+            for i = 1, self.count do
+                value = value * 1.15
+            end
+            return value
+        end
+    }),
+    Powerup({
+        name = 'Tough Skin',
+        description = 'Take 15% less damage'
+    })
 }

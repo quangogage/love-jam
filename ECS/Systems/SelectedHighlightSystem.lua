d0ca508b02ed264ba4b951ea0c90358c239699dd
Callgage <gageprod@gmail.com>
2024-02-22 17:23:35 -0800

    Fixed strange error in selectedhighlightsystem

--- ECS/Systems/SelectedHighlightSystem.lua
+++ ECS/Systems/SelectedHighlightSystem.lua
@@ -16,16 +16,18 @@ return function (concord)
     function SelectedHighlightSystem:draw()
         love.graphics.setColor(COLOR)
         for _, e in ipairs(self.entities) do
-            local image     = e:get('image').value
-            local offset    = e:get('offset') or { x = 0.5, y = 0.5 }
-            local scale     = e:get('scale') or { x = 1, y = 1 }
-            local increase = 1.2
-            love.graphics.setColor(0,1,0,1)
-            love.graphics.draw(image,
-                e.position.x, e.position.y - e.position.z,
-                0, scale.x*increase, scale.y*increase,
-                image:getWidth() * offset.x, image:getHeight() * offset.y
-            )
+            if e.image and e.offset and e.scale and e.position then
+                local image     = e:get('image').value
+                local offset    = e:get('offset') or { x = 0.5, y = 0.5 }
+                local scale     = e:get('scale') or { x = 1, y = 1 }
+                local increase = 1.2
+                love.graphics.setColor(0,1,0,1)
+                love.graphics.draw(image,
+                    e.position.x, e.position.y - e.position.z,
+                    0, scale.x*increase, scale.y*increase,
+                    image:getWidth() * offset.x, image:getHeight() * offset.y
+                )
+            end
         end
     end
 
